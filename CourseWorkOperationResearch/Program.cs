47887987b4c005132addaf585dcdfcd188ad9e7e
Art1ko <39236183+Art1ko@users.noreply.github.com>
2020-06-17 21:14:56 +0300

    Update Program.cs

--- CourseWorkOperationResearch/Program.cs
+++ CourseWorkOperationResearch/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Import.Helpers;
+using System;
 using System.Linq;
 
 namespace CourseWorkOperationResearch
@@ -7,14 +8,98 @@ namespace CourseWorkOperationResearch
     {
         static void Main(string[] args)
         {
-            var importedStructure = new Import.Import();
-            //var state = importedStructure.GetFullGraphFormFile(Import.Import.ReadImport(
-            //    @"D:\RiderProjects\CourseWorkOperationResearch\Import\import.json"));
-            var state = importedStructure.GenerateGraph(30, 21, true);
 
-            var greedySearchResult = new GreedySearch.GreedySearch().Run(state, 21);
-            var c = greedySearchResult.Sum(x => x.Cost);
-            var acoResult = new AntColonyOptimizationAlgorithm.AntColonyOptimization().Run(importedStructure.Graph, state, 22);
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            IndividualProblem individualProblem = new IndividualProblem();
+            Expirement exp = new Expirement();
+
+            while (true)
+            {
+                Console.Clear();
+                //Выводим меню, его пункты с соответствующими цифрами\символами
+                Console.WriteLine("### MENU ###");
+                Console.WriteLine("1. Індивідуальна задача");
+                Console.WriteLine("2. Експериментальне дослідження");
+                Console.WriteLine("3. Вихід");
+                Console.Write("\n" + "Введите команду: ");
+
+                uint.TryParse(Console.ReadLine(), out uint ch);
+                if (ch == 3)
+                    break;
+                switch (ch)
+                {
+                    case 1:
+
+                        IndividualProblemMenu();
+                        break;
+                    case 2:
+                        exp.SolveExpirementProblem();
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        Console.WriteLine("Введіть цифру від 1 до 8");
+                        Console.ReadLine();
+                        break;
+
+                }
+            }
+        }
+
+
+        private static void IndividualProblemMenu()
+        {
+            IndividualProblem individualProblem = new IndividualProblem();
+            while (true)
+            {
+                Console.Clear();
+                //Выводим меню, его пункты с соответствующими цифрами\символами
+                Console.WriteLine("### MENU ###");
+                Console.WriteLine("1. Встановити цінності міст");
+                Console.WriteLine("2. Встановити випадкові значення задачі");
+                Console.WriteLine("3. Змінити значення міста");
+                Console.WriteLine("4. Змінити значення шляху");
+                Console.WriteLine("5. Зберегти у individual.json");
+                Console.WriteLine("6. Зчитати з invdidual.json");
+                Console.WriteLine("7. Вирішити індивідуальну задачу");
+                Console.WriteLine("8. Вихід");
+                Console.Write("\n" + "Введіть команду: ");
+
+                uint.TryParse(Console.ReadLine(), out uint ch); //Тут желательно сделать проверку, или считывать всю строку, и в switch уже отсеивать
+                if (ch == 8)
+                    break;
+                
+                switch (ch)
+                {
+                    case 1:
+
+                        individualProblem.SetCitiesPrices();
+                        break;
+                    case 2:
+                        individualProblem.RandomIndividualProblem();
+                        break;
+                    case 3:
+                        individualProblem.EditCityPrices();
+                        break;
+                    case 4:
+                        individualProblem.SetPathLength();
+                        break;
+                    case 5:
+                        individualProblem.SaveToJson();
+                        break;
+                    case 6:
+                        individualProblem.ReadFromJson();
+                        break;
+                    case 7:
+                        individualProblem.SolveIndividualProblem();
+                        break;
+                    default:
+                        Console.WriteLine("Введіть цифру від 1 до 8");
+                        Console.ReadLine();
+                        break;
+
+                }
+            }
         }
     }
 }
